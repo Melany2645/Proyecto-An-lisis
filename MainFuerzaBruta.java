a8946ad64211a75a361004cdda3b32e7440e4cfd
Melany Jirón Díaz <m.jiron.2@estudiantec.cr>
2026-01-13 11:49:49 -0600

    Implementacion de piezas quemadas

--- MainFuerzaBruta.java
+++ MainFuerzaBruta.java
@@ -5,8 +5,8 @@ public class MainFuerzaBruta {
     public static void main(String []args) {
 
         // Parámetros del problema, Prueba con tamaño 3 y valores entre 0 y 9
-        int tamaño = 5;
-        int valorMaximo = 9;
+        int tamaño = 3;
+        int valorMaximo = 15;
 
         System.out.println("Iniciando prueba Fuerza Bruta");
         System.out.println("Tamaño del rompecabezas: " + tamaño + "x" + tamaño);
@@ -17,10 +17,31 @@ public class MainFuerzaBruta {
         Piezas generadorPiezas = new Piezas(tamaño, valorMaximo);
         ArrayList<Pieza> piezas = generadorPiezas.getPiezas();
 
+        // Piezas quemadas, primer tamaño 3x3
+        ArrayList<Pieza> piezasQuemadas = new ArrayList<>();
+        piezasQuemadas.add(new Pieza(1, 5, 4, 0));
+        piezasQuemadas.add(new Pieza(4, 3, 1, 5));
+        piezasQuemadas.add(new Pieza(0, 8, 2, 3));
+        piezasQuemadas.add(new Pieza(4, 2, 7, 5));
+        piezasQuemadas.add(new Pieza(1, 0, 3, 2));
+        piezasQuemadas.add(new Pieza(2, 8, 6, 0));
+        piezasQuemadas.add(new Pieza(7, 4, 3, 1));
+        piezasQuemadas.add(new Pieza(3, 1, 2, 4));
+        piezasQuemadas.add(new Pieza(6, 0, 5, 1));
 
-        System.out.println("Piezas generadas (desordenadas):");
-        for (Pieza pieza : piezas) {
-            System.out.println(pieza + " ");
+
+        if (tamaño == 3) {
+            System.out.println("Piezas quemadas:");
+            for (Pieza pieza : piezasQuemadas) {
+                System.out.println(pieza + " ");
+            }
+            // Usar las piezas quemadas cuando el tamaño sea 3
+            piezas = piezasQuemadas;
+        } else {
+            System.out.println("Piezas generadas (desordenadas):");
+            for (Pieza pieza : piezas) {
+                System.out.println(pieza + " ");
+            }
         }
         System.out.println("\n");
 
