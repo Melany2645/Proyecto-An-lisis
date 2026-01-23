b39e54e79f004aa9209114dff0b773c4c94aee88
Jeremy Alexander Montero Abarca <jmontero2107@gmail.com>
2026-01-23 15:04:17 -0600

    Clase posicion

---
+++ Posicion.java
@@ -0,0 +1,19 @@
+/**
+ * Clase que encuentra la posición.
+ * @autor Jeremy Montero
+ * @version 1.0
+ */
+public class Posicion {
+    public int fila;
+    public int columna;
+
+    /**
+     * Constructor de la clase Posición.
+     * @param fila Valor de la fila.
+     * @param columna Valor de la columna
+     */
+    public Posicion(int fila, int columna) {
+        this.fila = fila;
+        this.columna = columna;
+    }
+}
