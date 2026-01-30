1f12709b4712beb231a8ee21af10cff54f14fbd2
Melany Jirón Díaz <m.jiron.2@estudiantec.cr>
2026-01-30 02:17:09 -0600

    mediciones

--- MainGeneral.java
+++ MainGeneral.java
@@ -45,8 +45,8 @@ public class MainGeneral {
     public static void main(String[] args) {
 
         // Datos de entrada de prueba
-        int tamaño = 100; // Tamaño del tablero (3x3, 5x5, 10x10, ect.)
-        int valorMaximo = 9; 
+        int tamaño = 3; // Tamaño del tablero (3x3, 5x5, 10x10, ect.)
+        int valorMaximo = 15; 
 
         System.out.println("===== Proyecto Análisis de Algoritmos =====");
         System.out.println("Resolución de un rompecabezas mediante distintos algoritmos.");
